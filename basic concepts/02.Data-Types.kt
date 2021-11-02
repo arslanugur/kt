a9f80467c68f107f37fe9503c20a52c6169cd4d3
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 22:18:55 +0300

    Create 02.Data-Types.kt

---
+++ basic concepts/02.Data-Types.kt
@@ -0,0 +1,24 @@
+// Each value in Kotlin must have a type.
+// Some of the most used built-in data types are:
+          // Int indicates an integer (whole number),             such as 42.
+          // Double and Float are used to store decimal numbers,  such as 12.4.
+          // Char represents a character,                         such as 'z'.
+          // Boolean has two possible values, either true or false. 
+
+// https://kotlinlang.org/docs/basic-types.html
+
+// The text for a String is enclosed in double quotes.
+// The \n character can be used in a string to create a line break, for example:
+fun main(args : Array<String>) {
+    println("A \nB \nC") 
+} 
+
+/*
+Output:
+A 
+B 
+C
+*/
+
+// \n for line break \t for tab space
+
