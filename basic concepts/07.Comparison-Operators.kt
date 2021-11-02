de37d863b79b221a7d5e06bceaaf5645f60bcf3a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 22:50:13 +0300

    Create 07.Comparison-Operators.kt

---
+++ basic concepts/07.Comparison-Operators.kt
@@ -0,0 +1,34 @@
+// The comparison operators compare two values and return a boolean: true or false.
+// For example, 10>5 will return true.
+
+// Here are the comparison operators:
+// > greater than
+// < less than
+// >= greater than or equals to
+// <= less than or equals to
+// == is equal to
+// != is not equal
+
+// = to assign. == to compare
+
+// For example, you can check and output whether the age variable is greater than or equal to 16:
+fun main(args : Array<String>) {
+    var age = 18
+    println(age >= 16)
+} // output: true
+
+// The comparison operators are used in conditional statements, which will be described in a later lesson. 
+
+fun main(args: Array<String>) { 
+   var total = 200 
+   var articulos = 7 
+   println(total%articulos) 
+ }  // output: 4
+ 
+fun main(args: Array<String>) { 
+   var total = 200 
+   var div = 200/7 
+   println(total%div) 
+ }  // output: 4
+
+
